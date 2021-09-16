2da3707420b9dd3a627f10d1d30449c7723bedbb
Nova <eldanadesalgne@gmail.com>
2021-09-15 23:37:31 -0400

    final code modified

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,76 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//creating a user promt- to help with security and fit the needs of our client better
+var entryPrompt1 = Number (window.prompt ("Please enter the amount of characters you would like for your password: (must be between  8- 128)", " "));
+var pwd =[]; //created so that it can easly collect data. 
+
+// Defining Variables 
+var minPWL = 8; 
+var maxPWL = 128; 
+var letter = "abcdefghijklmnopqrstuvwxyz";
+var numbers ="0123456789"
+var specialChr =" !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var lcC = 0, ucC = 0, nC = 0, scC = 0; //counters
+var lcChar = "", ucChar = "", nChar = "", sChar = ""; //actual character
+var ranNum = 0;
+
+function wantLCF(){
+    //lcC = pwd.length(Math.floor(Math.random()));
+    var numL = letter.length(Math.floor(Math.random()));
+    letter.charAt(numL);
+}//lower character picking function 
+
+function wantUCF(){
+    var numL = letter.length(Math.floor(Math.random()));
+    letter.toUpperCase.charAt(numL);
+}//upper Character picking function 
+
+function wantNF(){
+    var numL = numbers.length(Math.floor(Math.random()));
+    numbers.charAt(numL);
+}// nuber picking function 
+
+function wantSCF(){
+    var numL = specialChr.length(Math.floor(Math.random()));
+    specialChr.charAt(numL);
+}// special character picking function 
+
+function randomGen(){
+  if      (ranNum === 0){wantLCF();}
+  else if (ranNum === 1){wantUCF();}
+  else if (ranNum === 2){wantNF();}
+  else {wantSCF();}
+} //  random array to call on functions 
+
+if (entryPrompt1 >= minPWL && entryPrompt1<= maxPWL) {
+  pwd.length = entryPrompt1; 
+} //This will make it so that the length will be this amount. 
+else {
+  alert ("Invalide response, please try reload and try again.");
+  return null; 
+} //asking users for length details 
+
+var wantLC = confirm("Would you like to add LowerCase letters to your password?");
+if (wantLC === true ){} else {wantLCF(ranNum = null);}
+var wantUC = confirm("Would you like to add UpperCase letters to your password?");
+if (wantUC === true){} else {wantUCF(ranNum = null);}
+var wantN  = confirm("Would you like to add Numbers to your password?");
+if (wantN === true){} else {wantNF(ranNum = null);}
+var wantSC = confirm("Would you like to add Special Characters to your password?");
+if (wantSC === true){} else {wantSCF(ranNum = null);}
+
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  passwordText.value = string; //object.value seems to return an array. 
 }
+generateBtn.addEventListener('click', writePassword)
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+for(i = 0; i <= pwd.length; i++)
+{ var arr = [0,1,2,3];
+  ranNum = arr.length(Math.floor(Math.random())); 
+  pwd [i] = randomGen(ranNum);
+  i++
+}// generate pass word 
+var string = pwd.toString();
+
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Password_Generator
+
+Dear employees, your request of having a random password generator has been completed. For detailed describtion, read bellow.
+
+## What is this webpage for?
+
+Like it's name, this website is powered by Java to help protect your companies sensitive information by providing you with secure passwords that will be the first defense in life against a cyber attack. 
+
+## How to operate/ navigate this page?
+
+The webpage is used in your browser. 
+
+## Screen shot of application
+
+Please view attachment in the assets folder as an attachment. 
+
+## "User Story"/ Critieria
+
+The employees asked for help on creating random passwords to secure data. That has been delivered. 
+
+Note that this following was what was specifically asked~ 
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
