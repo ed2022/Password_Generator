ff7452fddd16fbb3f17b9a6ccf8e7182fe6da31e
Nova <eldanadesalgne@gmail.com>
2021-09-16 00:30:46 -0400

    more update

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 //creating a user promt- to help with security and fit the needs of our client better
-var entryPrompt1 = Number (window.prompt ("Please enter the amount of characters you would like for your password: (must be between  8- 128)", " "));
+var entryPrompt1 = prompt ("Please enter the amount of characters you would like for your password: (must be between  8- 128)", "enter here");
+console.log(entryPrompt1)
+
+
 var pwd =[]; //created so that it can easly collect data. 
+pwd.length = entryPrompt1;
+console.log(pwd.length);
 
 // Defining Variables 
 var minPWL = 8; 
@@ -17,39 +22,36 @@ var ranNum = 0;
 
 function wantLCF(){
     //lcC = pwd.length(Math.floor(Math.random()));
-    var numL = letter.length(Math.floor(Math.random()));
+    var numL = (Math.floor(Math.random()*((letter.length-0)+1)+0)); 
     letter.charAt(numL);
+    console.log(letter.charAt(numL));
 }//lower character picking function 
 
 function wantUCF(){
-    var numL = letter.length(Math.floor(Math.random()));
-    letter.toUpperCase.charAt(numL);
+    var numL = (Math.floor(Math.random()*((letter.length-0)+1)+0)); 
+    letter.toUpperCase(letter.charAt(numL));
+    console.log(letter.toUpperCase(letter.charAt(numL)));
 }//upper Character picking function 
 
 function wantNF(){
-    var numL = numbers.length(Math.floor(Math.random()));
+    var numL = (Math.floor(Math.random()*((numbers.length-0)+1)+0)); 
     numbers.charAt(numL);
+    console.log(numbers.charAt(numL));
 }// nuber picking function 
 
 function wantSCF(){
-    var numL = specialChr.length(Math.floor(Math.random()));
+    var numL = (Math.floor(Math.random()*((specialChr.length-0)+1)+0)); 
     specialChr.charAt(numL);
+    console.log(specialChr.charAt(numL));
 }// special character picking function 
 
 function randomGen(){
-  if      (ranNum === 0){wantLCF();}
-  else if (ranNum === 1){wantUCF();}
-  else if (ranNum === 2){wantNF();}
+  if      (ranNum = 0){wantLCF();}
+  else if (ranNum = 1){wantUCF();}
+  else if (ranNum = 2){wantNF();}
   else {wantSCF();}
 } //  random array to call on functions 
 
-if (entryPrompt1 >= minPWL && entryPrompt1<= maxPWL) {
-  pwd.length = entryPrompt1; 
-} //This will make it so that the length will be this amount. 
-else {
-  alert ("Invalide response, please try reload and try again.");
-  return null; 
-} //asking users for length details 
 
 var wantLC = confirm("Would you like to add LowerCase letters to your password?");
 if (wantLC === true ){} else {wantLCF(ranNum = null);}
@@ -67,10 +69,14 @@ function writePassword() {
 generateBtn.addEventListener('click', writePassword)
 
 for(i = 0; i <= pwd.length; i++)
-{ var arr = [0,1,2,3];
-  ranNum = arr.length(Math.floor(Math.random())); 
-  pwd [i] = randomGen(ranNum);
+{ //var arr = [0,1,2,3];
+  //ranNum = arr.length(Math.floor(Math.random())); 
+  var ranNum= (Math.random()*((3-0)+1)+0); 
+  console.log(Math.floor(ranNum)); 
+  pwd [i] = randomGen(Math.floor(ranNum));
   i++
 }// generate pass word 
 var string = pwd.toString();
+console.log(string);
+console.log(pwd);
 
