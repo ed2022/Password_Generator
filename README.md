fda167592d8a4ec91ab0765b30a9124d1f5f5812
Nova <eldanadesalgne@gmail.com>
2021-10-03 19:25:57 -0400

    Fixed Issues

--- README.md
+++ README.md
@@ -8,11 +8,17 @@ Like it's name, this website is powered by Java to help protect your companies s
 
 ## How to operate/ navigate this page?
 
-The webpage is used in your browser. 
+The webpage is used in your browser. By clicking the Generate password Button, you are then prompted to put a in a number of specification to narrow down what you would like for the password generate to get a secure password. It has of "yes or cancel" questions. 
 
 ## Screen shot of application
 
 Please view attachment in the assets folder as an attachment. 
+
+## Links
+GitHub: https://github.com/ed2022/Password_Generator.git
+Webpage: https://ed2022.github.io/Password_Generator/
+
+Note that the HTML and CSS where already given. 
 
 ## "User Story"/ Critieria
 
--- script.js
+++ script.js
@@ -1,82 +1,45 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");//this is the id for the button 
 
-//creating a user promt- to help with security and fit the needs of our client better
-var entryPrompt1 = prompt ("Please enter the amount of characters you would like for your password: (must be between  8- 128)", "enter here");
-console.log(entryPrompt1)
+function generatePassword() {
+  var pwd = ""; //created so that it can easly collect data on a global scale to everythign else 
 
+  var letter = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789"
+  var specialChr = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~" //special characters from documentation 
+  var upperLetter = letter.toUpperCase(); //making it uppercase 
+  var finalPassword = "";
 
-var pwd =[]; //created so that it can easly collect data. 
-pwd.length = entryPrompt1;
-console.log(pwd.length);
+  var entryPrompt1 = prompt("Please enter the amount of characters you would like for your password: (must be between  8- 128)", "enter here");
+  console.log(entryPrompt1); //original prompt set up question. 
 
-// Defining Variables 
-var minPWL = 8; 
-var maxPWL = 128; 
-var letter = "abcdefghijklmnopqrstuvwxyz";
-var numbers ="0123456789"
-var specialChr =" !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-var lcC = 0, ucC = 0, nC = 0, scC = 0; //counters
-var lcChar = "", ucChar = "", nChar = "", sChar = ""; //actual character
-var ranNum = 0;
+  if ((parseInt(entryPrompt1) >= 8) && (parseInt(entryPrompt1) <= 128)) { //must meet this criteria to proceed 
 
-function wantLCF(){
-    //lcC = pwd.length(Math.floor(Math.random()));
-    var numL = (Math.floor(Math.random()*((letter.length-0)+1)+0)); 
-    letter.charAt(numL);
-    console.log(letter.charAt(numL));
-}//lower character picking function 
+    var wantLC = confirm("Would you like to add LowerCase letters to your password?");
+    if (wantLC === true) { pwd = pwd + letter; } //confirming the choice to add lower case letters 
+    var wantUC = confirm("Would you like to add UpperCase letters to your password?");
+    if (wantUC === true) { pwd = pwd + upperLetter; } //confirming the choice to add upper case letters 
+    var wantN = confirm("Would you like to add Numbers to your password?");
+    if (wantN === true) { pwd = pwd + numbers; } //confirming the choice to add numbers 
+    var wantSC = confirm("Would you like to add Special Characters to your password?");//confirming the choice to add special characters 
+    if (wantSC === true) { pwd = pwd + specialChr; }
 
-function wantUCF(){
-    var numL = (Math.floor(Math.random()*((letter.length-0)+1)+0)); 
-    letter.toUpperCase(letter.charAt(numL));
-    console.log(letter.toUpperCase(letter.charAt(numL)));
-}//upper Character picking function 
+    console.log(pwd);
 
-function wantNF(){
-    var numL = (Math.floor(Math.random()*((numbers.length-0)+1)+0)); 
-    numbers.charAt(numL);
-    console.log(numbers.charAt(numL));
-}// nuber picking function 
-
-function wantSCF(){
-    var numL = (Math.floor(Math.random()*((specialChr.length-0)+1)+0)); 
-    specialChr.charAt(numL);
-    console.log(specialChr.charAt(numL));
-}// special character picking function 
-
-function randomGen(){
-  if      (ranNum = 0){wantLCF();}
-  else if (ranNum = 1){wantUCF();}
-  else if (ranNum = 2){wantNF();}
-  else {wantSCF();}
-} //  random array to call on functions 
-
-
-var wantLC = confirm("Would you like to add LowerCase letters to your password?");
-if (wantLC === true ){} else {wantLCF(ranNum = null);}
-var wantUC = confirm("Would you like to add UpperCase letters to your password?");
-if (wantUC === true){} else {wantUCF(ranNum = null);}
-var wantN  = confirm("Would you like to add Numbers to your password?");
-if (wantN === true){} else {wantNF(ranNum = null);}
-var wantSC = confirm("Would you like to add Special Characters to your password?");
-if (wantSC === true){} else {wantSCF(ranNum = null);}
-
-function writePassword() {
-  var passwordText = document.querySelector("#password");
-  passwordText.value = string; //object.value seems to return an array. 
+    for (i = 0; i <= parseInt(entryPrompt1); i++) {
+      var randomNum = Math.floor(Math.random() * pwd.length);//pick a random number from the length of the arr and round up.
+      finalPassword = finalPassword + pwd.charAt(randomNum);//use that number and find the character at that index the add it to the password 
+    }
+    return finalPassword;//after all is complete, go ahead and return the final password all together
+  }
+  else {generatePassword()}//if criteria not matched, re- ask the first question
 }
-generateBtn.addEventListener('click', writePassword)
 
-for(i = 0; i <= pwd.length; i++)
-{ //var arr = [0,1,2,3];
-  //ranNum = arr.length(Math.floor(Math.random())); 
-  var ranNum= (Math.random()*((3-0)+1)+0); 
-  console.log(Math.floor(ranNum)); 
-  pwd [i] = randomGen(Math.floor(ranNum));
-  i++
-}// generate pass word 
-var string = pwd.toString();
-console.log(string);
-console.log(pwd);
-
+// password to the #password input
+function writePassword() {
+  var password = generatePassword(); // call function and assign the final password to the following variable
+  var passwordText = document.querySelector("#password"); // this is the id from the text area 
+  passwordText.value = password; //give the text area the value of the password
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); //when event listener "click" is confirmed, call function writepassword to do it's thing. 
